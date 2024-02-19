fc45342684329f3ee7edf1634e3e12fbb919b699
Krissapter <kevenrud@hotmail.no>
2024-02-19 15:20:36 +0100

    Added express middleware for BrowserRouter

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import * as path from "path";
 import {articleAPI} from "./articleAPI.js";
 
 const port = process.env.PORT || 3000;
@@ -10,5 +11,13 @@ app.listen(port);
 app.use(express.static("../client/dist"));
 app.use(bodyParser.json());
 app.use(articleAPI);
+
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
+});
 
 console.log(`Server started and is listening on  http://localhost:${port}`);
