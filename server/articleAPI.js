cf0da9c30119bd9a5e0d44a403b73228fa703469
Krissapter <kevenrud@hotmail.no>
2024-02-19 22:03:43 +0100

    Removed commented out code

--- server/articleAPI.js
+++ server/articleAPI.js
@@ -3,36 +3,6 @@ import express from "express";
 export const articleAPI = express.Router();
 
 export function createArticleRouter(db){
-    /*const ARTICLES = [
-        {
-            id: 1,
-            headline: "MERGE CONFLICT",
-            article: "Local student causes merge conflict, millions dead.",
-            category: "Tech",
-            author: "Test Testsson"
-        },
-        {
-            id: 2,
-            headline: "MAJOR BREAKTHROUGH",
-            article: "Monkey on typewriter finally decodes lorem ipsum, leaving researchers baffled",
-            category: "Literature",
-            author: "Chimp McNotAMonkey"
-        },
-        {
-            id: 3,
-            headline: "PARCEL ISSUES",
-            article: "Parcel kidnapped and refuses to install, gyp ERR suspected culprit",
-            category: "Tech",
-            author: "Test Testsson"
-        },
-        {
-            id: 4,
-            headline: "LOREM IPSUM",
-            article: "Lorem Ipsum Dolores etc",
-            category: "World",
-            author: "Printing Press"
-        }
-    ];*/
 
     articleAPI.get("/api/articles/", async (req, res) => {
         const articles = await db.collection("articles").find().limit(40).toArray();
