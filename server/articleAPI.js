714bdd3ab667c664ae14fb68ed1b13d9c9f4db50
Krissapter <kevenrud@hotmail.no>
2024-02-19 18:59:02 +0100

    Set up MongoDb

--- server/articleAPI.js
+++ server/articleAPI.js
@@ -2,43 +2,47 @@ import express from "express";
 
 export const articleAPI = express.Router();
 
-const ARTICLES = [
-    {
-        id: 1,
-        headline: "MERGE CONFLICT",
-        article: "Local student causes merge conflict, millions dead.",
-        category: "Tech",
-        author: "Test Testsson"
-    },
-    {
-        id: 2,
-        headline: "MAJOR BREAKTHROUGH",
-        article: "Monkey on typewriter finally decodes lorem ipsum, leaving researchers baffled",
-        category: "Literature",
-        author: "Chimp McNotAMonkey"
-    },
-    {
-        id: 3,
-        headline: "PARCEL ISSUES",
-        article: "Parcel kidnapped and refuses to install, gyp ERR suspected culprit",
-        category: "Tech",
-        author: "Test Testsson"
-    },
-    {
-        id: 4,
-        headline: "LOREM IPSUM",
-        article: "Lorem Ipsum Dolores etc",
-        category: "World",
-        author: "Printing Press"
-    }
-];
+export function createArticleRouter(db){
+    /*const ARTICLES = [
+        {
+            id: 1,
+            headline: "MERGE CONFLICT",
+            article: "Local student causes merge conflict, millions dead.",
+            category: "Tech",
+            author: "Test Testsson"
+        },
+        {
+            id: 2,
+            headline: "MAJOR BREAKTHROUGH",
+            article: "Monkey on typewriter finally decodes lorem ipsum, leaving researchers baffled",
+            category: "Literature",
+            author: "Chimp McNotAMonkey"
+        },
+        {
+            id: 3,
+            headline: "PARCEL ISSUES",
+            article: "Parcel kidnapped and refuses to install, gyp ERR suspected culprit",
+            category: "Tech",
+            author: "Test Testsson"
+        },
+        {
+            id: 4,
+            headline: "LOREM IPSUM",
+            article: "Lorem Ipsum Dolores etc",
+            category: "World",
+            author: "Printing Press"
+        }
+    ];*/
 
-articleAPI.get("/api/articles/", (req, res) => {
-    res.json(ARTICLES);
-});
+    articleAPI.get("/api/articles/", async (req, res) => {
+        const articles = await db.collection("articles").find().limit(40).toArray();
+        res.json(articles);
+    });
 
-articleAPI.post("/api/articles", (req, res) => {
-    const { headline, article, category, author } = req.body;
-    ARTICLES.push({ headline, article, category, author, id: ARTICLES.length });
-    res.sendStatus(204);
-});
+    articleAPI.post("/api/articles", async (req, res) => {
+        const { headline, article, category, author } = req.body;
+        await db.collection("articles").insertOne({ headline, article, category, author });
+        //ARTICLES.push({ headline, article, category, author, id: ARTICLES.length });
+        res.sendStatus(204);
+    });
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import * as path from "path";
-import {articleAPI} from "./articleAPI.js";
+import {articleAPI, createArticleRouter} from "./articleAPI.js";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,6 +15,15 @@ app.listen(port);
 app.use(express.static("../client/dist"));
 app.use(bodyParser.json());
 app.use(articleAPI);
+
+const url = process.env.MONGODB_URL;
+const dbClient = new MongoClient(url);
+
+dbClient.connect().then((connection) => {
+    const db = connection.db("article-db");
+    createArticleRouter(db);
+});
+
 
 app.use((req, res, next) => {
     if (req.method === "GET" && !req.path.startsWith("/api")) {
