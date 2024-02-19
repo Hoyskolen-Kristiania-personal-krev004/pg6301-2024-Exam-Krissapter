3c03ffc5216a2eafc4e7ef3322a3b5f5c0d30084
Krissapter <kevenrud@hotmail.no>
2024-02-19 11:55:47 +0100

    Initial commit with basic stuff

---
+++ client/components/application/application.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Application(){
+    return (
+        <h1>Hello World</h1>
+    );
+}
---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port);
+
+app.use(express.static("../client/dist"));
+
+console.log(`Server started and is listening on port ${port}`);
