6d035085da9b3ad2e558023c8a787d765002c03f
Krissapter <kevenrud@hotmail.no>
2024-02-21 04:45:42 +0100

    Gatekeeping adding news, but not as rigorously as would be preferable

--- client/components/article/addArticle.tsx
+++ client/components/article/addArticle.tsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from "react";
 import {ArticleContext} from "./articleContext";
 import {LoginContext} from "../login/loginContext";
+import {Login} from "../login/login";
 
 export function AddArticle(){
     const [headline, setHeadline] = useState("");
@@ -22,24 +23,34 @@ export function AddArticle(){
         setAuthor(username);
         setWebsocket(new WebSocket(window.location.origin.replace(/^http/, "ws")));
     }, []);
-    return(
-        <form onSubmit={handleSubmit}>
-            <h1>Add Article</h1>
+    if(!username){
+        return(
+            <form onSubmit={handleSubmit}>
+                <h1>Add Article</h1>
+                <div>
+                    Headline:<br/>
+                    <input value={headline} onChange={(e) => setHeadline(e.target.value)} required/><br/>
+                    Article:<br/>
+                    <textarea value={article} onChange={(e) => setArticle(e.target.value)} required/><br/>
+                    Category:<br/>
+                    <select name={category} defaultValue={"Tech"} onChange={(e) => setCategory(e.target.value)} >
+                        <option value={"Tech"}>Tech</option>
+                        <option value={"Literature"}>Literature</option>
+                        <option value={"World"}>World</option>
+                    </select><br/>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
+        );
+    } else {
+        return (
             <div>
-                Headline:<br/>
-                <input value={headline} onChange={(e) => setHeadline(e.target.value)} required/><br/>
-                Article:<br/>
-                <textarea value={article} onChange={(e) => setArticle(e.target.value)} required/><br/>
-                Category:<br/>
-                <select name={category} defaultValue={"Tech"} onChange={(e) => setCategory(e.target.value)} >
-                    <option value={"Tech"}>Tech</option>
-                    <option value={"Literature"}>Literature</option>
-                    <option value={"World"}>World</option>
-                </select><br/>
+                <h3>Please log in to read articles</h3><br/>
+                <Login />
             </div>
-            <div>
-                <button>Submit</button>
-            </div>
-        </form>
-    );
+        );
+    }
+
 }
